130a0cb0f2a10c0a3e122e918b2a8c8c49d78b65
Anders Steiner <anders.steiner@gmail.com>
2015-03-05 23:28:04 +0100

    add example test

---
+++ src/test/java/com/github/andidev/dronetest/WebDriverExtensionsExampleTest.java
@@ -0,0 +1,40 @@
+package com.github.andidev.dronetest;
+
+import static com.github.webdriverextensions.Bot.assertCurrentUrlContains;
+import static com.github.webdriverextensions.Bot.assertTextContains;
+import static com.github.webdriverextensions.Bot.click;
+import static com.github.webdriverextensions.Bot.open;
+import static com.github.webdriverextensions.Bot.type;
+import static com.github.webdriverextensions.Bot.waitFor;
+import com.github.webdriverextensions.junitrunner.WebDriverRunner;
+import com.github.webdriverextensions.junitrunner.annotations.Firefox;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@RunWith(WebDriverRunner.class)
+@Firefox
+public class WebDriverExtensionsExampleTest {
+
+    // Model
+    @FindBy(name = "q")
+    WebElement queryInput;
+    @FindBy(name = "btnG")
+    WebElement searchButton;
+    @FindBy(id = "search")
+    WebElement searchResult;
+
+    @Test
+    public void searchGoogleForHelloWorldTest() {
+        open("http://www.google.com");
+        assertCurrentUrlContains("google");
+
+        type("Hello World", queryInput);
+        click(searchButton);
+
+        waitFor(3, SECONDS);
+        assertTextContains("Hello World", searchResult);
+    }
+}
